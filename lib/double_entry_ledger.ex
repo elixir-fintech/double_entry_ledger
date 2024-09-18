e1f7fb0063a515e483bd4e60f52a402011b9602b
Christian Sommerauer <csommerauer@outfarm.com>
2024-09-18 15:28:17 +0100

    intial commit

---
+++ lib/double_entry_ledger.ex
@@ -0,0 +1,18 @@
+defmodule DoubleEntryLedger do
+  @moduledoc """
+  Documentation for `DoubleEntryLedger`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DoubleEntryLedger.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/double_entry_ledger_test.exs
@@ -0,0 +1,8 @@
+defmodule DoubleEntryLedgerTest do
+  use ExUnit.Case
+  doctest DoubleEntryLedger
+
+  test "greets the world" do
+    assert DoubleEntryLedger.hello() == :world
+  end
+end
