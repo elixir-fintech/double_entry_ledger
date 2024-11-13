261bb967ee41558bf75046804eab645151b53176
Christian Sommerauer <csommerauer@outfarm.com>
2024-11-13 17:13:38 +0000

    chore: moved to simpler OCC implementation, also mix format

--- lib/double_entry_ledger/workers/event_worker/create_event.ex
+++ lib/double_entry_ledger/workers/event_worker/create_event.ex
@@ -4,11 +4,16 @@ defmodule DoubleEntryLedger.CreateEvent do
   """
 
   alias Ecto.Multi
+
   alias DoubleEntryLedger.{
-    Event, Transaction, EventStore, TransactionStore, Repo
+    Event,
+    Transaction,
+    EventStore,
+    TransactionStore,
+    Repo
   }
 
-  import DoubleEntryLedger.OccRetry, only: [retry: 2]
+  import DoubleEntryLedger.OccRetry
   import DoubleEntryLedger.EventTransformer, only: [transaction_data_to_transaction_map: 2]
 
   @doc """
@@ -16,25 +21,55 @@ defmodule DoubleEntryLedger.CreateEvent do
 
   Returns `{:ok, transaction, event}` on success, or `{:error, reason}` on failure.
   """
-  @spec process_create_event(Event.t()) :: {:ok, Transaction.t(), Event.t()} | {:error, String.t()}
+  @spec process_create_event(Event.t()) ::
+          {:ok, Transaction.t(), Event.t()} | {:error, String.t()}
   def process_create_event(%Event{transaction_data: transaction_data, instance_id: id} = event) do
     case transaction_data_to_transaction_map(transaction_data, id) do
       {:ok, transaction_map} ->
-        retry(&create_transaction_and_update_event/2, [event, transaction_map])
+        create_event(event, transaction_map)
+
       {:error, error} ->
         EventStore.mark_as_failed(event, error)
         {:error, error}
     end
   end
 
+  @spec create_event(Event.t(), map()) ::
+          {:ok, {Transaction.t(), Event.t()}} | {:error, String.t()}
+  defp create_event(event, transaction_map) do
+    retry_create_event(event, transaction_map, max_retries())
+  end
+
+  @spec retry_create_event(Event.t(), map(), integer()) ::
+          {:ok, {Transaction.t(), Event.t()}} | {:error, String.t()}
+  defp retry_create_event(event, transaction_map, attempts) when attempts > 0 do
+    try do
+      create_transaction_and_update_event(event, transaction_map)
+    rescue
+      Ecto.StaleEntryError ->
+        {:ok, updated_event} = EventStore.add_error(event, occ_error_message(attempts))
+        set_delay_timer(attempts)
+        retry_create_event(updated_event, transaction_map, attempts - 1)
+    end
+  end
+
+  defp retry_create_event(_event, _transaction_map, 0) do
+    {:error, occ_final_error_message()}
+  end
+
   @spec create_transaction_and_update_event(Event.t(), map()) ::
-    {:ok, {Transaction.t(), Event.t()}} | {:error, String.t()}
+          {:ok, {Transaction.t(), Event.t()}} | {:error, String.t()}
   defp create_transaction_and_update_event(event, transaction_map) do
     case build_create_transaction_and_update_event(event, transaction_map)
-    |> Repo.transaction() do
-      {:ok, %{transaction: transaction, event: update_event}} -> {:ok, {transaction, update_event}}
-      {:error, step, error, _} -> {:error, "#{step} failed: #{error}"}
-      {:error, error} -> {:error, "#{error}"}
+         |> Repo.transaction() do
+      {:ok, %{transaction: transaction, event: update_event}} ->
+        {:ok, {transaction, update_event}}
+
+      {:error, step, error, _} ->
+        {:error, "#{step} failed: #{error}"}
+
+      {:error, error} ->
+        {:error, "#{error}"}
     end
   end
 
@@ -43,7 +78,7 @@ defmodule DoubleEntryLedger.CreateEvent do
     Multi.new()
     |> TransactionStore.build_create(:transaction, transaction_map)
     |> Multi.update(:event, fn %{transaction: td} ->
-        EventStore.build_mark_as_processed(event, td.id)
-      end)
+      EventStore.build_mark_as_processed(event, td.id)
+    end)
   end
 end
