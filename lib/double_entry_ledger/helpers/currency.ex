9452ebfb45fc40ffaccb21839ffb6c4dfa36091f
Christian Sommerauer <csommerauer@outfarm.com>
2025-03-26 13:29:02 +0000

    chore: add to_money

--- lib/double_entry_ledger/helpers/currency.ex
+++ lib/double_entry_ledger/helpers/currency.ex
@@ -53,20 +53,52 @@ defmodule DoubleEntryLedger.Currency do
 
     - A `Money.t()` struct with an absolute value of the amount.
   """
-  # credo:disable-for-next-line Credo.Check.Warning.SpecWithStruct
-  @spec to_abs_money(integer(), currency_atom() | binary()) :: %Money{:amount => integer(), :currency => currency_atom()} | {:error, String.t()}
-  def to_abs_money(amount, currency) when is_integer(amount) and currency in @currency_atoms do
-    Money.new(abs(amount), currency)
+  @spec to_abs_money(integer(), currency_atom() | binary()) :: Money.t() | {:error, String.t()}
+  def to_abs_money(amount, currency) when is_integer(amount) do
+    to_money(abs(amount), currency)
   end
 
-  def to_abs_money(amount, currency) when is_integer(amount) and is_binary(currency) do
+  def to_abs_money(_amount, _currency), do: {:error, "Invalid amount or currency"}
+
+
+  @doc """
+  Converts an amount and a currency to a `Money` struct.
+
+  ## Parameters
+
+    - `amount` - The integer amount to convert.
+    - `currency` - The currency atom for the amount.
+
+  ## Examples
+
+      iex> Currency.to_money(-100, :USD)
+      %Money{amount: -100, currency: :USD}
+
+      iex> Currency.to_money(100, "USD")
+      %Money{amount: 100, currency: :USD}
+
+      iex> Currency.to_money(100, "XYZ")
+      {:error, "Invalid currency"}
+
+      iex> Currency.to_money("100", 100)
+      {:error, "Invalid amount or currency"}
+
+  ## Returns
+
+    - A `Money.t()` struct with an absolute value of the amount.
+  """
+  @spec to_money(integer(), currency_atom() | binary()) :: Money.t()| {:error, String.t()}
+  def to_money(amount, currency) when is_integer(amount) and currency in @currency_atoms do
+    Money.new(amount, currency)
+  end
+
+  def to_money(amount, currency) when is_integer(amount) and is_binary(currency) do
     try do
-      Money.new(abs(amount), String.to_existing_atom(currency))
+      to_money(amount, String.to_existing_atom(currency))
     rescue ArgumentError ->
       {:error, "Invalid currency"}
     end
   end
 
-  def to_abs_money(_amount, _currency), do: {:error, "Invalid amount or currency"}
-
+  def to_money(_amount, _currency), do: {:error, "Invalid amount or currency"}
 end
