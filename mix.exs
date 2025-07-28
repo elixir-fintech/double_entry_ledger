eaeb920c2fecacbca931c54289c7d5a2f228e950
Christian Sommerauer <csommerauer@outfarm.com>
2025-07-28 17:40:00 +0100

    adding tidewave

--- mix.exs
+++ mix.exs
@@ -38,7 +38,9 @@ defmodule DoubleEntryLedger.MixProject do
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.1", only: [:dev], runtime: false},
       {:mox, "~> 1.0", only: [:test]},
-      {:ex_doc, "~> 0.34", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.34", only: :dev, runtime: false},
+      {:tidewave, "~> 0.2", only: :dev},
+      {:bandit, "~> 1.0", only: :dev},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
@@ -51,7 +53,8 @@ defmodule DoubleEntryLedger.MixProject do
 
   defp aliases do
     [
-      test: ["ecto.create --quiet", "ecto.migrate", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate", "test"],
+      tidewave: "run --no-halt -e 'Agent.start(fn -> Bandit.start_link(plug: Tidewave, port: 4000) end)'",
     ]
   end
 
