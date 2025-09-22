fc20840d8837c583d9ef3250628d4b072de0e094
Christian Sommerauer <csommerauer@outfarm.com>
2025-09-22 12:52:36 +0100

    allow account without transactions to be deleted

--- priv/repo/migrations/2200_add_event_account_links_table.exs
+++ priv/repo/migrations/2200_add_event_account_links_table.exs
@@ -5,7 +5,7 @@ defmodule DoubleEntryLedger.Repo.Migrations.AddEventAccountLinksTable do
     create table(:event_account_links, primary_key: false, prefix: "double_entry_ledger") do
       add :id, :binary_id, primary_key: true
       add :event_id, references(:events, on_delete: :nothing, type: :binary_id), null: false
-      add :account_id, references(:accounts, on_delete: :nothing, type: :binary_id), null: false
+      add :account_id, references(:accounts, on_delete: :delete_all, type: :binary_id), null: false
 
       timestamps(type: :utc_datetime_usec)
     end
